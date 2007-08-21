370503cfcb196c4a3a22fca0870198b9c0c081c8
mike <mike@b13a2f67-ab02-0c40-833e-274245c7f63f>
2007-08-21 22:57:56 +0000

    Added accessories to do matrix3x3 and 3D vector maths.
    git-svn-id: svn://10.0.50.55/twisted/trunk@3 b13a2f67-ab02-0c40-833e-274245c7f63f

--- src/Geometry/Matrix.h
+++ src/Geometry/Matrix.h
@@ -32,6 +32,22 @@ inline const Matrix2x1 operator * ( const Matrix2x1& _a, const Matrix2x2& _b ) {
 	return Temp;		
 }
 // - ------------------------------------------------------------------------------------------ - //
+inline const Matrix1x3 operator * ( const Matrix3x3& _a, const Matrix1x3& _b ) {
+	Matrix1x3 Temp;
+	Temp.x = (_a.Array[ 0 ] * _b.x) + (_a.Array[ 1 ] * _b.y) + (_a.Array[ 2 ] * _b.z);
+	Temp.y = (_a.Array[ 3 ] * _b.x) + (_a.Array[ 4 ] * _b.y) + (_a.Array[ 5 ] * _b.z);
+	Temp.z = (_a.Array[ 6 ] * _b.x) + (_a.Array[ 7 ] * _b.y) + (_a.Array[ 8 ] * _b.z);
+	return Temp;	
+}
+// - ------------------------------------------------------------------------------------------ - //
+inline const Matrix3x1 operator * ( const Matrix3x1& _a, const Matrix3x3& _b ) {
+	Matrix3x1 Temp;
+	Temp.x = (_a.x * _b.Array[ 0 ]) + (_a.y * _b.Array[ 3 ]) + (_a.z * _b.Array[ 6 ]);
+	Temp.y = (_a.x * _b.Array[ 1 ]) + (_a.y * _b.Array[ 4 ]) + (_a.z * _b.Array[ 7 ]);
+	Temp.z = (_a.x * _b.Array[ 2 ]) + (_a.y * _b.Array[ 5 ]) + (_a.z * _b.Array[ 8 ]);
+	return Temp;		
+}
+// - ------------------------------------------------------------------------------------------ - //
 
 // - ------------------------------------------------------------------------------------------ - //
 // Apply a matrix transformation to a vector //
@@ -42,11 +58,11 @@ inline const Vector2D Vector2D::ApplyMatrix( const Matrix2x2& _Vs ) {
 	return Temp;
 }
 // - ------------------------------------------------------------------------------------------ - //
-//inline const Vector2D Vector2D::ApplyMatrix( const Matrix3x3& _Vs ) {
-//	Vector2D Temp;
-//	Temp = (this->ToMatrix3x1() * _Vs).ToVector2D();
-//	return Temp;
-//}
+inline const Vector3D Vector3D::ApplyMatrix( const Matrix3x3& _Vs ) {
+	Vector3D Temp;
+	Temp = (this->ToMatrix3x1() * _Vs).ToVector3D();
+	return Temp;
+}
 // - ------------------------------------------------------------------------------------------ - //
 
 // - ------------------------------------------------------------------------------------------ - //
